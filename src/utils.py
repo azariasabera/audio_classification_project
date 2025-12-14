fdf49f1a72f313f4a49b6901a96e778a2ba7c9df
Azarias Galama <azariasabera@gmail.com>
2025-12-14 09:06:07 +0200

    Add utility function to save and load trained models

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 # src/utils.py
 
+import joblib
+from pathlib import Path
 from omegaconf import DictConfig
 from typing import Any
 import pandas as pd
@@ -36,3 +38,43 @@ def split_data(X: pd.DataFrame, y: pd.Series, config: DictConfig) -> tuple:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_proportion, stratify=y, random_state=42)
     
     return X_train, X_test, y_train, y_test
+
+def save_model(model: Any, config: DictConfig, model_name: str) -> Path:
+    """
+    Save the trained model to a specified directory with the given extension.
+
+    Args:
+        model: The trained model to save.
+        config: Hydra config object (for getting output directory).
+        model_name: Name to use for the saved model file (without extension).
+
+    Returns:
+        Path: The full path to the saved model file.
+    """
+    # if 'output/dir' doesn't exist default with /output in cwd
+    output_dir = Path(getattr(config, "output", {}).get("model_dir", "./output"))
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # I don't use extension = getattr(config.output, "model_extension", ".joblib") to avoid potential error in config.ouput
+    extension = getattr(getattr(config, "output", {}), "model_extension", ".joblib") # defaults to .joblib
+
+    model_path = output_dir / (model_name + extension)
+
+    joblib.dump(model, model_path)
+    #print(f"Model saved to {model_path}")
+
+    return model_path
+
+def load_model(model_path: Path) -> Any:
+    """
+    Load a model from a specified file.
+
+    Args:
+        model_path: Path to the saved model file.
+
+    Returns:
+        The loaded model.
+    """
+    model = joblib.load(model_path)
+    print(f"Model loaded from {model_path}")
+    return model
